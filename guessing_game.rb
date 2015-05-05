92a2e7fcf7d163ecdb6702fe227d157c709fd9bb
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-05 15:57:07 -0400

    It works but its not complete

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,41 +1,49 @@
-
-
 def analyzer(answer)
   guesses = []
-  (0..5).each do
-    if guesses.length >= 5
+  count = 0
+  (0..5).each do |a|
+    if a == 5
       puts "You didnt guess it in enouph tries, you lose."
       return
-    user_guess = gets.chomp
+    end
+    user_guess = gets.chomp.to_i
     if guesses.include?(user_guess)
       responder("same")
-    end
-    guesses << user_guess
-    if user_guess == answer
+    elsif user_guess == answer
       responder("win")
       return
-    else 
-      responder(calculator(user_guess, answer))
+    else
+      responder(calculater(user_guess, answer))
+    end
+    guesses << user_guess
   end
 end
 
 def calculater(guess, answer)
   if guess < answer
     return "low"
-  else return "high"
+  else
+    return "high"
+  end
 end
 
 def responder(type)
   if type == "same"
-    puts "You said that already. TEEHEE I remembered what you guessed and u didnt."
+    puts "You said that already. TEEHEE I remembered what you guessed and u didnt. Guess again"
+  end
   if type == "high"
     puts "Your too high, guess again"
+  end
   if type == "low"
-    puts "Your too low guess again"
+    puts "Your too low, guess again"
+  end
   if type == "win"
     puts "You have won the game, congradulations"
+  end
 end
 
 comp_num = rand (1..100)
+puts comp_num
 puts "I chose a number between 1 and 100, can you guess the number?"
 analyzer(comp_num)
+puts "hi"
