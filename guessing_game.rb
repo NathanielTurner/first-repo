9dd6dc7f034730b83ca544ea8630c86fbb33aca5
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-05 13:55:18 -0400

    building framework and adding autmated responses

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,2 +1,16 @@
+
+
+def(guess, answer)
+  guesses = []
+
+end
+
+def
+  puts "You said that already. TEEHEE I remembered what you guessed and u didnt."
+  puts "Your too high, guess again"
+  puts "Your too low guess again"
+end
+
 comp_num = rand (1..100)
 puts "I chose a number between 1 and 100, can you guess the number?"
+user_guess = gets.chomp
